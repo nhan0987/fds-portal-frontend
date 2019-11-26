798fecfe651f3b2695c745c9065a0d28eaebf2ca
ThanhNV <nguyenthanh.ptiter@gmail.com>
2019-11-26 15:16:48 +0700

    split components FAQ

--- portal-app/src/views/faq/FAQ.vue
+++ portal-app/src/views/faq/FAQ.vue
@@ -1,87 +1,29 @@
 <template>
-  <v-layout class="faq" wrap>
-    <v-flex xs12 sm7>
+  <v-row class="faq" wrap>
+    <v-col xs12 sm7>
       Danh sach cau hoi
-      <HelloWorld />
-    </v-flex>
-    <v-flex xs12 sm5>
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <v-text-field
-          v-model="name"
-          :counter="10"
-          :rules="nameRules"
-          label="Name"
-          required
-        ></v-text-field>
-
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          label="E-mail"
-          required
-        ></v-text-field>
-
-        <v-select
-          v-model="select"
-          :items="items"
-          :rules="[v => !!v || 'Item is required']"
-          label="Item"
-          required
-        ></v-select>
-
-        <v-checkbox
-          v-model="checkbox"
-          :rules="[v => !!v || 'You must agree to continue!']"
-          label="Do you agree?"
-          required
-        ></v-checkbox>
-
-        <v-btn
-          :disabled="!valid"
-          color="success"
-          class="mr-4"
-          @click="validate"
-        >
-          Validate
-          <v-icon right dark>mdi-cloud-upload</v-icon>
-        </v-btn>
-
-        <v-btn color="error" class="mr-4" @click="reset">
-          Reset Form
-        </v-btn>
-
-        <v-btn color="warning" @click="resetValidation">
-          Reset Validation
-        </v-btn>
-      </v-form>
-    </v-flex>
-  </v-layout>
+      <ListFAQ />
+    </v-col>
+    <v-col xs12 sm5>
+      Đặt câu hỏi
+      <AdminEditFAQ />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ListFAQ from "@/components/faq/ListFAQ.vue";
+import AdminEditFAQ from "@/components/faq/AdminEditFAQ.vue";
 
 export default {
   name: "faq",
   components: {
-    HelloWorld
+    ListFAQ,
+    AdminEditFAQ
   },
   data: () => ({
-    valid: true,
-    name: "",
-    nameRules: [
-      v => !!v || "Name is required",
-      v => (v && v.length <= 10) || "Name must be less than 10 characters"
-    ],
-    email: "",
-    emailRules: [
-      v => !!v || "E-mail is required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-    ],
-    select: null,
-    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    checkbox: false
+    valid: true
   }),
   created() {
     // TODO: get list Question
