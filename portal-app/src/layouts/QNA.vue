45926f57aa988ee332fb8e47085a1a23ff79c6ff
ThanhNV <nguyenthanh.ptiter@gmail.com>
2019-11-26 10:39:25 +0700

    portal init project

---
+++ portal-app/src/layouts/QNA.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app dark>
+    <v-content>
+      <v-container fluid fill-height>
+        <transition>
+          <keep-alive>
+            <router-view></router-view>
+          </keep-alive>
+        </transition>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "QNA",
+  components: {}
+};
+</script>
